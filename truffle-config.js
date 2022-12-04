86094ff297b518800328dd8444925dc218358dbd
abdulsagheer <abdulsagheeras29@gmail.com>
2022-12-04 23:02:35 +0530

    Added truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -7,4 +7,11 @@ module.exports = {
       version: '0.8.16'
     }
   },
+  networks: {
+    inf_Ebay_goerli: {
+      network_id: 5,
+      gasPrice: 100000000000,
+      provider: new HDWalletProvider(fs.readFileSync('/Users/abdulsagheer/Downloads/Untitled.env', 'utf-8'), "https://goerli.infura.io/v3/a4581e8331494e06869205553225d1a6")
+    }
+  }
 };
